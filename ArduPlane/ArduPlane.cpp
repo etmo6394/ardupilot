536314c9a3144a787bc9cebb695e7fc96bf7e4c7
etmo6394 <ethan.morgan@colorado.edu>
2018-01-20 19:49:39 -0700

    1

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -163,7 +163,7 @@ void Plane::RAMROD_Switch()
 
     // gcs().send_text(MAV_SEVERITY_INFO, "agc | agc_prev: %d | %d  (#: %d).", (int)agc_feedback, (int)agc_feedback_prev, (int)randswitch);
 
-    auto agc = ahrs.get_agc_feedback();
+    auto agc = ahrs.get_agc_feedback(void);
 
     agc_feedback_prev = agc.x;
     agc_feedback = agc.y;
