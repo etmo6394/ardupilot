819562bd35455e1186ac9652fd2faf0a05c81d34
etmo6394 <ethan.morgan@colorado.edu>
2018-02-27 12:57:44 -0700

    1

--- libraries/AP_InertialSensor/AP_InertialSensor_DMU11.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor_DMU11.cpp
@@ -127,6 +127,8 @@ void AP_InertialSensor_DMU11::accumulate(void)
         // read byte from buffer
         if (nbytes == 1) {
             nbytes = uart->available();
+            //need to delay 95.5 us per framed byte
+            hal.scheduler->delay_microseconds(191);
             hal.console->printf("nbytes: %d\n", nbytes);
         }
         c = uart->read();
